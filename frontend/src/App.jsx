764bf3986b02ab6bebc317b946463a5bb985c309
CHANDOO <chandoomalik2013@gmail.com>
2026-07-06 18:31:54 +0500

    fix videoPreview crash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,6 +37,8 @@ export default function App() {
 
   const [previewUrl, setPreviewUrl] = useState("");
   
+  const [videoPreview, setVideoPreview] = useState("")
+  
   useEffect(() => {
 
   return () => {
